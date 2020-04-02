fb8440e079df975db244ae2180169d778130fa82
Marine Rougier <m.rougier@hotmail.fr>
2020-04-02 13:39:01 +0200

    js

--- js/firebase-credentials.js
+++
@@ -1,5 +0,0 @@
-// set parameters to log data to a keen.io project.
-var firebase_config = {
-	apiKey: "AIzaSyD-IWnSOjWQY1DHq4928U_RZfDg0PvM-mg",
-	databaseURL: "https://stage-marine.firebaseio.com"
-}
