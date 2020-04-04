7d2d6d1a57f78679bb23a2d58461824979eee96e
Marine Rougier <m.rougier@hotmail.fr>
2020-04-04 18:22:19 +0200

    data_analyses

--- data_analyses/1_DataExtraction_VAAST.R
+++ data_analyses/1_DataExtraction_VAAST.R
@@ -25,6 +25,22 @@ dataset_vaast_trial <-
          temp_data = map(temp_data, ~ fromJSON(.x))) %>% 
   unnest()
 
+# email dataset ---------------------------------------------------
+dataset_email <-
+  database %>% 
+  pluck("email_contacts") %>% 
+  map_dfr(~data_frame(email = .x$email,
+                      info_requested = .x$info_requested,
+                      language = .x$language),
+          .id = "id") %>%
+  group_by(id) %>%
+  arrange(desc(epoch)) %>% 
+  filter(row_number() == 1) %>% 
+  ungroup() %>% 
+  mutate(timestamp = lubridate::as_datetime(epoch / 1000),
+         temp_data = map(temp_data, ~ fromJSON(.x))) %>% 
+  unnest()
+
 # browser event dataset ---------------------------------------------------
 dataset_vaast_browser_event <-
   database %>% 
@@ -133,26 +149,58 @@ dataset_extra <-
   ungroup() %>%
   distinct()
 
-# language info -------------------------------------------------------------
-dataset_language <-
+# questions info -------------------------------------------------------------
+dataset_questions <-
   database %>%
-  pluck("language_info_corona_en") %>%
+  pluck("questions_info_corona_en") %>%
   map_dfr(~data_frame(epoch = .x$timestamp,
                       jspsych_id = .x$jspsych_id,
                       vaast_first_block = .x$vaast_first_block,
-                      temp_data = .x$language_data),
+                      temp_data = .x$questions_data),
           .id = "id") %>%
   mutate(timestamp = lubridate::as_datetime(epoch / 1000),
          temp_data = map(temp_data, ~ fromJSON(.x))) %>%
   unnest() %>%
   subset(jspsych_id != "999")%>%
-  #drop_na(responses) %>%
-  #mutate(temp_data = map(responses, ~ fromJSON(.x))) %>%
+  drop_na(responses) %>%
+  mutate(temp_data = map(responses, ~ fromJSON(.x))) %>%
   group_by(jspsych_id) %>%
-  mutate(language = button_pressed) %>%
-  select(jspsych_id, language) %>%
+  mutate(item_1 = item_1,
+         item_2 = item_2,
+         item_3 = item_3,
+         item_4 = item_4,
+         item_5 = item_5,
+         item_6 = item_6,
+         item_7 = item_7,
+         item_8 = item_8,
+         item_9 = item_9,
+         item_10 = item_10,
+         item_11 = item_11,
+         item_12 = item_12,
+         item_13 = item_13,
+         item_14 = item_14,
+         item_15 = item_15,
+         item_16 = item_16,
+         item_17 = item_17) %>%
+  select(jspsych_id, item_1, item_2, item_3, item_4, item_5, item_6, item_7, item_8, item_9, item_10, item_11, item_12, item_13, item_14, item_15, item_16, item_17) %>%
   rowwise() %>%
-  mutate(language = pluck(language, 1)) %>%
+  mutate(item_1 = pluck(item_1, 1),
+         item_2 = pluck(item_2, 1),
+         item_3 = pluck(item_2, 1),
+         item_4 = pluck(item_4, 1),
+         item_5 = pluck(item_5, 1),
+         item_6 = pluck(item_6, 1),
+         item_7 = pluck(item_7, 1),
+         item_8 = pluck(item_8, 1),
+         item_9 = pluck(item_9, 1),
+         item_10 = pluck(item_10, 1),
+         item_11 = pluck(item_11, 1),
+         item_12 = pluck(item_12, 1),
+         item_13 = pluck(item_13, 1),
+         item_14 = pluck(item_14, 1),
+         item_15 = pluck(item_15, 1),
+         item_16 = pluck(item_16, 1),
+         item_17 = pluck(item_17, 1)) %>%
   ungroup() %>%
   distinct()
 
